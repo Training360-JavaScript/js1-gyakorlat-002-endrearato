8af4e7d146c8ccec0bfa74d1149abe59eef7229c
endrearato <endre.arato.81@gmail.com>
2022-01-13 16:58:28 +0100

    done

--- js/first.js
+++ js/first.js
@@ -5,13 +5,13 @@
 - A függvény szorozza meg a tömbelemek értékét 1.27-tel, majd pedig 
 __térjen vissza__ az elemek egész számra kerekített összegével!
 */
-const numArray = [5, 15, 25, 35];
+const numArray = [4, 15, 25, 35];
 
 
 const brutto = numArray
     .map(item  => item * 1.27)
     .reduce((previousNumber, currentNumber) => previousNumber + currentNumber)
-let result = Math.round(brutto)
+const result = Math.round(brutto)
 
 console.log(result)
 console.log(typeof(result))
